3f3808c72198ef2298f6737727ca2be38fc7d1f4
unknown <Administrador@EXT-004.hvitarte.gob.pe>
2018-12-31 16:02:05 -0500

    version 3

--- boletin/models.py
+++ boletin/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Registrado(models.Model):
+	nombre = models.CharField(max_length=100, blank=True, null=True)
+	email = models.EmailField()
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+	def __unicode__(self):
+		return self.email		
