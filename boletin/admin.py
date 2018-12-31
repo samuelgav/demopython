eeebc391f87ea085aa78d4a38acebce4e632e648
unknown <Administrador@EXT-004.hvitarte.gob.pe>
2018-12-31 17:06:58 -0500

    version 5

--- boletin/admin.py
+++ boletin/admin.py
@@ -6,5 +6,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Registrado
 
+class AdminRegistrado(admin.ModelAdmin):
+	list_display = ["email", "nombre", "timestamp"]
+	list_filter = ["timestamp"]
+	list_editable = ["nombre"]
+	list_fields = ["email", "nombre"]
+	class Meta:
+		model = Registrado
+		
 
-admin.site.register(Registrado)
+admin.site.register(Registrado, AdminRegistrado)
