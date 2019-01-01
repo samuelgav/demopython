32d029407e15a77622e9f5a6889e98de2d1e087a
unknown <Administrador@EXT-004.hvitarte.gob.pe>
2019-01-01 11:20:17 -0500

    new version 1

--- boletin/forms.py
+++ boletin/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
-	edad = forms.IntegerField()
+	email = forms.EmailField()
--- boletin/views.py
+++ boletin/views.py
@@ -4,10 +4,18 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from .forms import RegForm
+from .models import Registrado
 
 # Create your views here.
 def inicio(request):
-	form = RegForm()
+	form = RegForm(request.POST or None)
+	if form.is_valid():
+		form_data = form.cleaned_data
+		abc = form_data.get("email")	
+		abc2 = form_data.get("nombre")
+		obj = Registrado.objects.create(email=abc, nombre=abc2)
+
+
 	context = {
 		"el_form": form,
 	}
