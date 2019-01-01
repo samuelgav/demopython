34edb4fb6b186ee1bfe2b15827d4c4d76a3aa857
unknown <Administrador@EXT-004.hvitarte.gob.pe>
2019-01-01 11:39:55 -0500

    new version 2

--- boletin/admin.py
+++ boletin/admin.py
@@ -4,15 +4,17 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .forms import RegModelForm
 from .models import Registrado
 
 class AdminRegistrado(admin.ModelAdmin):
 	list_display = ["email", "nombre", "timestamp"]
+	form = RegModelForm
 	list_filter = ["timestamp"]
 	list_editable = ["nombre"]
 	list_fields = ["email", "nombre"]
-	class Meta:
-		model = Registrado
+	#class Meta:
+	#	model = Registrado
 		
 
 admin.site.register(Registrado, AdminRegistrado)
--- boletin/forms.py
+++ boletin/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 
+from .models import Registrado
+
+class RegModelForm(forms.ModelForm):
+	class Meta:
+		model = Registrado
+		fields = ["nombre", "email"]		
+
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
 	email = forms.EmailField()
