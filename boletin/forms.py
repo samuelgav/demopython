81b270454b5f0a2fad19a5beddd4cf768950a016
unknown <Administrador@EXT-004.hvitarte.gob.pe>
2019-01-03 09:40:38 -0500

    nuevo commit

--- boletin/forms.py
+++ boletin/forms.py
@@ -5,7 +5,20 @@ from .models import Registrado
 class RegModelForm(forms.ModelForm):
 	class Meta:
 		model = Registrado
-		fields = ["nombre", "email"]		
+		fields = ["nombre", "email"]	
+
+	def clean_email(self):
+		email = self.cleaned_data.get("email")	
+		email_base, proveedor = email.split("@")
+		dominio, extension = proveedor.split(".")
+		if not extension == "edu":
+			raise forms.ValidationError("Por favor utiliza un email con la extension .edu")
+		return email
+
+	def clean_nombre(self):
+		nombre = self.cleaned_data.get("nombre")
+		#validaciones
+		return nombre
 
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
